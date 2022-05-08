f68a8af2c5e8d581f5553447a38575743582be58
albelito <albelito98@gmail.com>
2022-05-08 17:40:32 +0000

    squeezesegv3 initial code

--- models/encoder.py
+++ models/encoder.py
@@ -13,3 +13,9 @@ class BaseEncoder (nn.Module):
 def get_salsa_encoder(num_inchannel=6):
     from .SalsaNext import SalsaNextEncoder
     return SalsaNextEncoder(num_inchannel)
+
+
+def get_squeeze_encoder(num_inchannel=6, drop_prob=0.25):
+    from .SqueezeSegv3 import SqueezeSegEncoder
+    return SqueezeSegEncoder(num_inchannel, drop_prob)
+
